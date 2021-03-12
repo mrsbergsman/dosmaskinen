852eb9c9d13cf838e05609d6b1fcb30b19af10fc
Ann Sellén <brevtillann@hotmail.com>
2021-03-12 09:55:11 +0100

    holabaloo

---
+++ src/components/register/Register.js
@@ -0,0 +1,156 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import { withRouter } from "react-router-dom";
+import './_register.scss'
+import {API_BASE_URL, ACCESS_TOKEN_NAME} from '../../constants/apiConstants';
+import './_register.scss'
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import RegisterHeader from '../registerHeader/RegisterHeader';
+//import AlertComponent from '../AlertComponent/AlertComponent'; 
+
+
+function Register(props) {
+
+    // Sets state object to email and password setState will update the value
+    const [state , setState] = useState({
+        email : "",
+        password : "",
+        confirmPassword: "",
+        successMessage: null
+    })
+
+    const [title, updateTitle] = useState(null);
+    // const [errorMessage, updateErrorMessage] = useState(null);
+
+    //function that handle values when writing in input fields
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+
+    //function that check if password match in both password inputs.
+    //function send a call to sendDetailsToserver function to make a backend request. 
+    //Else show a message it do not match.
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        if(state.password === state.confirmPassword) {
+            sendDetailsToServer()    
+        } else {
+            props.showError('Passwords do not match');
+        }
+    }
+
+    //Making a post request using Axios to the server where API_BASE_URL is defined in the constans file.
+    const sendDetailsToServer = () => {
+        if(state.email.length && state.password.length) {
+            props.showError(null);
+            const payload={
+                "email":state.email,
+                "password":state.password,
+            } 
+            axios.post(API_BASE_URL+'/user/register', payload)
+                .then(function (response) {
+                    if(response.status === 200){
+                        setState(prevState => ({
+                            ...prevState,
+                            'successMessage' : 'Registration successful. Redirecting to home page..'
+                        }))
+                        redirectToHome();
+                        props.showError(null)
+                    } else{
+                        props.showError("Some error ocurred");
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });    
+        } else {
+            props.showError('Please enter valid username and password')    
+        }
+        
+    }
+    //sends user to home on succesful regristration on click
+    const redirectToHome = () => {
+        props.updateTitle('Home')
+        props.history.push('/start');
+    }
+
+    //redirect to login component insted
+    const redirectToLogin = () => {
+        props.updateTitle('Login')
+        props.history.push('/login'); 
+    }
+
+  return (
+    <>
+    <Header/>
+    <div className="register-container">
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+
+        <RegisterHeader title={title}/> 
+            <form>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" 
+                        className="form-control" 
+                        id="email" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter email..."
+                        value={state.email}
+                        onChange={handleChange}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder="Password..."
+                        value={state.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputPassword1">Confirm Password</label>
+                    <input type="password" 
+                        className="form-control" 
+                        id="confirmPassword" 
+                        placeholder="Confirm password..."
+                        value={state.confirmPassword}
+                        onChange={handleChange} 
+                    />
+                </div>
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                    onClick={handleSubmitClick}
+                >
+                    Register
+                </button>
+            </form>
+            {/* <Register showError={updateErrorMessage} updateTitle={updateTitle}/> */}
+            {/* <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>  */}
+
+        </div>
+
+        <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none' }} role="alert">
+            {state.successMessage}
+        </div>
+        <div className="account-text">
+            <span>Already have an account? </span>
+            <span className="loginText" onClick={() => redirectToLogin()}>Login here</span> 
+        </div>
+
+    </div>
+
+    <Footer/>
+    </>
+  );
+}
+
+export default withRouter(Register);
--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -2,17 +2,101 @@ import React from 'react';
 import {NavLink} from 'react-router-dom'
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-
 import './_start.scss'
+import startImage from '../../images/mainIconGreen.png';
+import dosmaskin from '../../images/dosmaskin.png';
+import onePill from '../../images/medIconTransp.png';
+import clock from '../../images/clockIconTransp.png';
+import message from '../../images/messIconTransp.png';
 
 function Start() {
   return (
     <>
     <Header/>
     <div className="start-container">
-      <h1>Start</h1>
-      <p>First page. Little info about the machine</p>
-      <NavLink  to="/login"> log in or sign up </NavLink>
+      <div className="top-box">
+        <div className="row">
+          <div className="col">
+            <img className="start-image" src={startImage} alt="Two pills on green bottom"/>
+          </div>
+          <div className="col">
+            <p className="welcome-text">Welcome to</p>
+            <p className="welcome-underText">Medical dosing machine</p>
+            <div className="navigation">
+      <NavLink className="header-nav" exact to="/login"> 
+        <li className='header-link'>Log in</li>
+      </NavLink>
+      <NavLink className="header-nav" exact to="/register"> 
+        <li className='header-link'>Register</li>
+      </NavLink>
+    </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom-box">
+        <div className="section1-box">
+          <div className="row">            
+            <div className="col">
+                <p className="information-header">What is Medical dosing machine?</p>
+                <p className="information-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+                  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                  anim id est laborum."
+                  <br/><br/>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+                  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                  anim id est laborum."
+                  <br/><br/>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+                  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                  anim id est laborum."
+                </p>
+            </div>
+            <div className="col">
+              <img className="machine-image" src={dosmaskin} alt="The dosing machine"/>
+            </div>
+          </div>            
+        </div>
+      </div>
+
+      <div>
+        <div className="bottom-box2">
+          <div className="section1-box">
+            <p className="bottom-box2-header">How does it work?</p>
+            <div class="row">
+              <div class="col">
+                <img className="start-icons" src={onePill} alt="A pill icon"/>
+                <p className="start-icon-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  </p>
+              </div>
+              <div class="col">                
+                <img className="start-icons" src={clock} alt="A clock icon"/>
+                <p className="start-icon-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  </p>
+              </div>
+              <div class="col">
+                <img className="start-icons" src={message} alt="A message icon"/>
+                <p className="start-icon-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
     <Footer/>
     </>
