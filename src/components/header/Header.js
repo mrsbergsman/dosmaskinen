7e3e3af31dae702ba214dec3824a32c405d16cd5
”Maria <thea_79@hotmail.com>
2021-04-13 16:32:18 +0200

    wip

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import {NavLink} from 'react-router-dom'
 import './_header.scss'
 
 
@@ -8,16 +7,7 @@ function Header(){
   <div className="header-container">
     <div className="logo-box">
       <p className="logo">MedicMinder</p>
-      {/* <p className="logo-underText">the medication reminder</p> */}
     </div>
-    {/* <div className="navigation">
-      <NavLink className="header-nav" exact to="/login"> 
-        <li className='header-link'>Log in</li>
-      </NavLink>
-      <NavLink className="header-nav" exact to="/login"> 
-        <li className='header-link'>Create an account</li>
-      </NavLink>
-    </div> */}
   </div>
   )
 }
