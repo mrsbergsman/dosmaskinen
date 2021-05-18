c1b073ae4bb3e3df9453e98cbaaa6f0eab6628b9
”Maria <thea_79@hotmail.com>
2021-05-18 12:47:10 +0200

    wip

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,43 @@
+
+
 import React from 'react';
 import UserInfo from '../registerHeader/userInfo';
-
-
-
+import './_header.scss'
 const Header = ()=>{
   return (
     <>
+      <div className="header-container">
+        <div className="logo-box">
+          <p className="logo">MedicMinder</p>
+        </div>
+      </div>
       <UserInfo/>
     </>
   )
 }
 export default Header
+
+
+
+
+
+// import React from 'react';
+// import UserInfo from '../registerHeader/userInfo';
+// import './_header.scss'
+// const Header = ()=>{
+//   return (
+//     <>
+//       <div className="header-container">
+//         <div className="logo-box">
+//           <p className="logo">MedicMinder</p>
+//         </div>
+//       </div>
+//       <UserInfo/>
+//     </>
+//   )
+// }
+// export default Header
+
+
+
+
--- src/components/registerHeader/userInfo.js
+++ src/components/registerHeader/userInfo.js
@@ -1,3 +1,26 @@
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './_userinfo.scss'
+// const UserInfo = ({isLoggedIn,userName, onLogout}) => (
+//   <>
+//     <div className="user-container">
+//         {isLoggedIn && <p className="user-info">
+//             <span className="user-badge">
+//             Welcome {userName}!
+//             </span>
+//             {isLoggedIn && <Link type="link" onClick={onLogout} className="logout" >
+//             logout  </Link>}
+//           </p>}
+//       </div>
+//     </>
+// );
+// export default UserInfo;
+
+
+
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './_userinfo.scss'
@@ -5,7 +28,6 @@ import './_userinfo.scss'
 const UserInfo = ({isLoggedIn,userName, onLogout}) => (
  
     <div className="user-wrapper">
-      <div className="logo">MedicMinder</div>
       <div className="user-container">
           {isLoggedIn && <p className="user-info">
               <span className="user-badge">
