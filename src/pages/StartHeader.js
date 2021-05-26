853dd276a8f1d252286fc6445d54ad617c4c2b6f
”Maria <thea_79@hotmail.com>
2021-05-26 13:19:27 +0200

    added header in register and login

---
+++ src/pages/StartHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './_startHeader.scss'; 
+
+function StartHeader () {
+  return(
+    <div className="startHeader-container">
+      <h1 className="startHeader-logo">MedicMinder</h1>
+    </div>
+  )
+}
+export default StartHeader;
